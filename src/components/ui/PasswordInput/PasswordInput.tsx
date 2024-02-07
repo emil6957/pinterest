d8d9062aedd62e86c264d53de56156c7ae8cf813
Emil <emiliano.milewski@gmail.com>
2024-02-07 23:39:55 +0000

    password component

---
+++ src/components/ui/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PasswordInput = ({ }) => {
+    return (
+        <div>
+            <label htmlFor="password">Password</label>
+            <input placeholder="Password" type="password" name="password" id="password" />
+        </div>
+    );
+}
+
+export default PasswordInput;
