cb3ddae69703156080b6559ffafc11bc9cbc05c9
Emil <emiliano.milewski@gmail.com>
2024-02-10 22:37:12 +0000

    gave classnames

--- src/components/ui/PasswordInput/PasswordInput.tsx
+++ src/components/ui/PasswordInput/PasswordInput.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./PasswordInput.scss";
 
 const PasswordInput = ({ }) => {
     return (
-        <div>
-            <label htmlFor="password">Password</label>
-            <input placeholder="Password" type="password" name="password" id="password" />
+        <div className="password-input">
+            <label className="password-input__label" htmlFor="password">Password</label>
+            <input className="password-input__input" placeholder="Password" type="password" name="password" id="password" />
         </div>
     );
 }
