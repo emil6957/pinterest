a9fb4c735055b0c2f3ffe7c0739b5d4ebd55d742
Emil <emiliano.milewski@gmail.com>
2024-02-12 22:17:42 +0000

    changed styling/added new component

--- src/components/AuthenticationPopUp/AuthenticationPopUp.tsx
+++ src/components/AuthenticationPopUp/AuthenticationPopUp.tsx
@@ -1,17 +1,22 @@
-import "./AuthenticationPopUp.scss";
 import React from "react";
+import "./AuthenticationPopUp.scss";
 import Button from "../ui/Button/Button";
 import LoginForm from "../LoginForm/LoginForm";
 import AuthTop from "../AuthTop/AuthTop"; 
+import { ReactComponent as GoogleIcon} from "../../assets/images/google-icon.svg";
+import SignupForm from "../SignupForm/SignupForm";
+import AuthBottom from "../AuthBottom/AuthBottom";
 
 const AuthenticationPopUp = ({ }) => {
-
     return (
         <div className="authentication-popup">
-            <AuthTop />
-            <LoginForm />
-            <p>OR</p>
-            <Button title={"Continue with Google"} style={"google"} />
+            <div className="authentication-popup__wrapper">
+                <AuthTop />
+                {false ? <LoginForm /> : <SignupForm />}
+                <p>OR</p>
+                <Button title={"Continue with Google"} style={"google"} icon={{img: <GoogleIcon />, position: "end"}}/>
+                <AuthBottom />
+            </div>
         </div>
     )
 }
