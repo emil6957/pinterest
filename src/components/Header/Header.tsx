abb8feef1a52d789a81fc8318cb15764b58df4ed
Emil <emiliano.milewski@gmail.com>
2024-03-02 22:32:00 +0000

    class names / styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Header.scss";
 import { ReactComponent as Logo } from "../../assets/images/logo.svg";
 
 type HeaderProps = {
@@ -8,25 +9,30 @@ type HeaderProps = {
 const Header = ({}:HeaderProps) => {
     return (
         <header className="header">
-            <div className="header__home">
-                <button className="header__home-btn">
-                    <Logo />
-                    <h1>Pinterest</h1>
-                </button>
+            <div className="header__wrapper">
+                <div className="header__home">
+                    <button className="header__home-btn">
+                        <Logo />
+                        <h1 className="header__home-btn-text">Pinterest</h1>
+                    </button>
+                </div>
+                <div className="header__links-wrapper">
+                    <button className="header__btn">Today</button>
+                    <button className="header__btn">Watch</button>
+                    <button className="header__btn">Explore</button>
+                </div>
             </div>
-            <div className="header__btns">
-                <button>Today</button>
-                <button>Watch</button>
-                <button>Explore</button>
-            </div>
-            <div className="header__btns">
-                <button>About</button>
-                <button>Business</button>
-                <button>Blog</button>
-            </div>
-            <div className="header__authentication">
-                <button>Log in</button>
-                <button>Sign in</button>
+
+            <div className="header__wrapper">
+                <div className="header__links-wrapper">
+                    <button className="header__btn">About</button>
+                    <button className="header__btn">Business</button>
+                    <button className="header__btn">Blog</button>
+                </div>
+                <div className="header__authentication">
+                    <button>Log in</button>
+                    <button>Sign in</button>
+                </div>
             </div>
         </header>
     )
