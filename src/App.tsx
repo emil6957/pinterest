5a88a243f4c7444adc7f2b4c558ffa66904343ec
Emil <emiliano.milewski@gmail.com>
2024-03-02 22:11:39 +0000

    imported header into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import AuthenticationPopUp from './components/AuthenticationPopUp/AuthenticationPopUp';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <AuthenticationPopUp></AuthenticationPopUp>
     </div>
   );
