37fdc65e26334a3b3b67120f7f9ddae9edae4dc3
Emil <emiliano.milewski@gmail.com>
2024-03-17 20:23:20 +0000

    Clicking outside the popup while its open closes it

--- src/components/AuthenticationPopUp/AuthenticationPopUp.tsx
+++ src/components/AuthenticationPopUp/AuthenticationPopUp.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./AuthenticationPopUp.scss";
 import Button from "../ui/Button/Button";
 import LoginForm from "../LoginForm/LoginForm";
@@ -7,16 +7,34 @@ import { ReactComponent as GoogleIcon} from "../../assets/images/google-icon.svg
 import SignupForm from "../SignupForm/SignupForm";
 import AuthBottom from "../AuthBottom/AuthBottom";
 import useToggle from "../../hooks/useToggle";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../services/states/store";
+import { toggle } from "../../services/states/ShowPopUpSlice/ShowPopUpSlice";
 
 const AuthenticationPopUp = ({}) => {
     const showPopUp = useSelector((state: RootState) => state.ShowPopup.value);
+    const dispatch = useDispatch();
     const [isSigningUp, toggleIsSigningUp] = useToggle(false);
 
+    const popupBackgroundRef: any = useRef(null);
+    const popupRef: any = useRef(null);
+    useEffect(() => {
+        const handler = (e: any) => {
+            if(!popupRef.current.contains(e.target) && popupBackgroundRef.current.contains(e.target)) {
+                dispatch(toggle());
+            }
+        };
+
+        document.addEventListener("mousedown", handler);
+
+        return () => {
+            document.removeEventListener("mousedown", handler)
+        };
+    });
+
     return (
-        <div className="authentication-popup" style={{display: showPopUp ? "block" : "none"}}>
-            <div className="authentication-popup__wrapper">
+        <div className="authentication-popup" style={{display: showPopUp ? "block" : "none"}} ref={popupBackgroundRef}>
+            <div className="authentication-popup__wrapper" ref={popupRef}>
                 <AuthTop />
                 {isSigningUp ? <LoginForm /> : <SignupForm />}
                 <p>OR</p>
