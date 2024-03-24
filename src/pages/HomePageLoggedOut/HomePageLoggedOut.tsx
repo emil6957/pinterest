020827feeb2d8d205f71eb79140d194df5ab9ea9
emil6957 <emiliano.milewski@gmail.com>
2024-03-24 22:24:35 +0000

    Added basic setup for rest of homepageloggedout

--- src/pages/HomePageLoggedOut/HomePageLoggedOut.tsx
+++ src/pages/HomePageLoggedOut/HomePageLoggedOut.tsx
@@ -16,6 +16,27 @@ const HomePageLoggedOut = ({ }:HomePageLoggedOutProps) => {
                 <Header />
             </Provider>
             <Hero />
+
+            <section className="home-page-logged-out__section">
+                <div className="home-page-logged-out__img"/>
+                <p className="home-page-logged-out__text">
+                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+                </p>
+            </section>
+
+            <section className="home-page-logged-out__section">
+                <div className="home-page-logged-out__img"/>
+                <p className="home-page-logged-out__text">
+                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+                </p>
+            </section>
+
+            <section className="home-page-logged-out__section">
+                <div className="home-page-logged-out__img"/>
+                <p className="home-page-logged-out__text">
+                    Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+                </p>
+            </section>
         </div>
     )
 }
