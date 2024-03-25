87dac10545725e0417914d54d05fba5a96dbb9c4
emil6957 <emiliano.milewski@gmail.com>
2024-03-25 00:13:23 +0000

    Put HeaderPageLoggedOut component into app to test it manually

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import AuthenticationPopUp from './components/AuthenticationPopUp/AuthenticationPopUp';
 import Header from './components/Header/Header';
+import HomePageLoggedOut from './pages/HomePageLoggedOut/HomePageLoggedOut';
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
-      <AuthenticationPopUp></AuthenticationPopUp>
+        <Header />
+        <AuthenticationPopUp/>
+        <HomePageLoggedOut />
     </div>
   );
 }
