3f7c886ef92e57e66bba334f0837a45139a31b9c
emil6957 <emiliano.milewski@gmail.com>
2024-03-25 00:12:30 +0000

    Added scroll snapping to HomePageLoggedOut component

--- src/pages/HomePageLoggedOut/HomePageLoggedOut.tsx
+++ src/pages/HomePageLoggedOut/HomePageLoggedOut.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "./HomePageLoggedOut.scss";
 import Hero from "../../components/Hero/Hero";
-import Header from "../../components/Header/Header";
-import { Provider } from "react-redux";
-import { store } from "../../services/states/store";
 
 type HomePageLoggedOutProps = {
 
@@ -12,11 +9,9 @@ type HomePageLoggedOutProps = {
 const HomePageLoggedOut = ({ }:HomePageLoggedOutProps) => {
     return (
         <div className="home-page-logged-out">
-            <Provider store={store}>
-                <Header />
-            </Provider>
-            <Hero />
-
+            <section className="home-page-logged-out__hero">
+                <Hero />
+            </section>
             <section className="home-page-logged-out__section">
                 <div className="home-page-logged-out__img"/>
                 <p className="home-page-logged-out__text">
