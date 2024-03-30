877e3d014cb3fd4e0694b0981ebec615973264a1
emil6957 <emiliano.milewski@gmail.com>
2024-03-30 23:44:08 +0000

    Created new functionality that resets the timer once u click on a carousel button

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,43 +9,57 @@ type HeroProps = {
 
 }
 
-const Hero = ({}:HeroProps) => {
-    const [carouselText, setCarouselText] = useState(["Test","Second Test", "Another Test", "Last Test",]);
-    const [carouselSelection, setCarouselSelection] = useState(0);
+const Hero = ({ }: HeroProps) => {
+    const [carouselText, setCarouselText] = useState<string[]>(["Test", "Second Test", "Another Test", "Last Test",]);
+    const [carouselSelection, setCarouselSelection] = useState<number>(0);
+    const [carouselTimerId, setCarouselTimerId] = useState<number>(0);
+    const [carouselIntervalDuration, setCarouselIntervalDuration] = useState<number>(6000);
 
-    useEffect(() => {
-      const nextCarouselItem = () => {
+    const nextCarouselItem = (): void => {
         setCarouselSelection(prevState => {
-            if( prevState === carouselText.length-1 ) {
+            if (prevState === carouselText.length - 1) {
                 return 0;
-            } else { 
-                return prevState+1
+            } else {
+                return prevState + 1
             }
         });
-      }
+    }
 
-      const timer = setInterval(nextCarouselItem, 6000)
+    // ! Using window.setInterval to avoid a bug of different types when using variable func
+    const resetTimer = (timer: number, func: Function, ms: number) => {
+        clearInterval(timer);
+        const newTimer: number = window.setInterval(func, ms);
+        setCarouselTimerId(newTimer);
+    }
+    
+    const handleCarouselButton = (i: number, timer: number, func: Function, ms: number) => {
+        setCarouselSelection(i);
+        resetTimer(timer, func, ms);
+    }
 
-      return () => {
-          clearInterval(timer);
-      }
-    }, [])
+    useEffect(() => {
+        const timer: number = window.setInterval(nextCarouselItem, carouselIntervalDuration);
+        setCarouselTimerId(timer);
 
+        return () => {
+            clearInterval(timer);
+        }
+    }, []);
 
     let Cards = [];
 
     const columns = 7;
     const rows = 3;
-    for(let i = 0; i < columns*rows; i++) {
+    for (let i = 0; i < columns * rows; i++) {
         Cards.push(<div key={i} className="hero__card-container"><Card size="sm" img={image} /></div>)
     }
 
-    const carouselTitles = carouselText.map((curr, i) => 
+    const carouselTitles = carouselText.map((curr, i) =>
         <li key={i}><p className={`hero__title hero__carousel-text ${carouselSelection === i ? "active" : ""}`}>{curr}</p></li>
     )
 
-    const carouselButtons = carouselText.map((curr, i) => 
-        <li key={i}><button onClick={() => setCarouselSelection(i)} className={`hero__carousel-button ${carouselSelection === i ? "active" : ""}`}/></li>
+    const carouselButtons = carouselText.map((curr, i) =>
+        <li key={i}><button onClick={() => handleCarouselButton(i, carouselTimerId, nextCarouselItem, carouselIntervalDuration)} className={`hero__carousel-button ${carouselSelection === i ? "active" : ""}`} /></li>
     );
 
     return (
