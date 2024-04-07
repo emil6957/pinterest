9bf6fa5ab114fae9a86e3f8a50bbf174e7f0db7e
emil6957 <emiliano.milewski@gmail.com>
2024-04-07 01:40:28 +0100

    prettier used on file

--- src/pages/HomePageLoggedOut/HomePageLoggedOut.tsx
+++ src/pages/HomePageLoggedOut/HomePageLoggedOut.tsx
@@ -1,36 +1,46 @@
 import React from "react";
 import "./HomePageLoggedOut.scss";
 import Hero from "../../components/Hero/Hero";
+import Footer from "../../components/Footer/Footer";
+import AuthenticationPopUp from "../../components/AuthenticationPopUp/AuthenticationPopUp";
 
 type HomePageLoggedOutProps = {
 
 }
 
-const HomePageLoggedOut = ({ }:HomePageLoggedOutProps) => {
+const HomePageLoggedOut = ({ }: HomePageLoggedOutProps) => {
     return (
         <div className="home-page-logged-out">
             <section className="home-page-logged-out__hero">
                 <Hero />
             </section>
             <section className="home-page-logged-out__section">
-                <div className="home-page-logged-out__img"/>
+                <div className="home-page-logged-out__img" />
                 <p className="home-page-logged-out__text">
                     Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
                 </p>
             </section>
 
             <section className="home-page-logged-out__section">
-                <div className="home-page-logged-out__img"/>
+                <div className="home-page-logged-out__img" />
                 <p className="home-page-logged-out__text">
                     Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
                 </p>
             </section>
 
             <section className="home-page-logged-out__section">
-                <div className="home-page-logged-out__img"/>
+                <div className="home-page-logged-out__img" />
                 <p className="home-page-logged-out__text">
                     Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
                 </p>
+            </section>
+
+            <section className="home-page-logged-out__section">
+                <div className="home-page-logged-out__call-to-action">
+                    <p className="home-page-logged-out__text">Sign up now and explore!</p>
+                    <AuthenticationPopUp />
+                </div>
+                <Footer />
             </section>
         </div>
     )
